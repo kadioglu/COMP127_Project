267d6ace214ed653b2e8f4f69a1dbcaad8035a70
lizjac <lizbet.jacobo@gmail.com>
2020-04-26 21:27:34 -0500

    Added scanner and win/lose methods

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -12,6 +12,7 @@ public class Hangman {
     private Random random;
     private ArrayList<String> usedLetters;
     private static int guessesLeft = 6;
+    private Scanner scan = new Scanner(System.in);
 
     public Hangman() {
         startGame();
@@ -25,7 +26,8 @@ public class Hangman {
      */
     private void playAgain(String response) {
         if (response.equalsIgnoreCase("y")) {
-            startGame();
+            System.out.println("We're playing again");
+            // startGame();
         }
         else {
             System.out.println("Game Over. Thanks for playing");
@@ -37,11 +39,13 @@ public class Hangman {
      */
     private void startGame() {
         //pick random word--can be method or through random generator here
-        String hiddenWord = "chocolate"; // for testing
+        String hiddenWord = "vanilla"; // for testing
         for (int i = 0; i < hiddenWord.length(); i++) {
             System.out.print(" - ");
         }
         System.out.println("\nYou have " + guessesLeft + " guesses left");
+
+        win(); // here for testing purposes
 
         userGuesses();
     }
@@ -54,6 +58,23 @@ public class Hangman {
 
     }
 
+    /**
+     * This method is to signal the player that they won and asks if 
+     * they want to play again
+     */
+    private void win() {
+        System.out.println("Congrats! You won! Would you like to play again? (Type \"y\" or \"n\")");
+       playAgain(scan.nextLine());
+    }
+
+    /**
+     * This method is to signal the player that they lost and asks if 
+     * they want to play again
+     */
+    private void lose() {
+        System.out.println("I'm sorry, but you lost. Would you like to play again? (Type \"y\" or \"n\")");
+       playAgain(scan.nextLine());
+    }
 
     public static void main (String args[]) {
         Hangman game = new Hangman();
